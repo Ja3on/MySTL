1e6f70632c186315fcc711a36d66be12891d4bdc
Ja3on <ja3on.he@gmail.com>
2016-06-13 08:32:05 +0800

    重构

--- Detail/AVLTree.impl.h
+++ Detail/AVLTree.impl.h
@@ -8,8 +8,8 @@ namespace MySTL
 	namespace Detail
 	{
 		template <typename T>
-		avl_iter<T>::avl_iter(const T* ptr,cntrPtr container)
-			:__ptr(ptr),__container(container)
+		avl_iter<T>::avl_iter(const T* ptr, cntrPtr container)
+			:__ptr(ptr), __container(container)
 		{
 			if (!__ptr) return;
 			auto temp = __container->__root;
@@ -65,13 +65,17 @@ namespace MySTL
 			++*this;
 			return temp;
 		}
-		
+
 		template <typename T>
-		bool operator==(const avl_iter<T>& lhs,const avl_iter<T>& rhs)
-		{	return lhs.__ptr == rhs.__ptr;	}
+		bool operator==(const avl_iter<T>& lhs, const avl_iter<T>& rhs)
+		{
+			return lhs.__ptr == rhs.__ptr;
+		}
 		template <typename T>
-		bool operator!=(const avl_iter<T>& lhs,const avl_iter<T>& rhs)
-		{	return !(lhs == rhs);	}
+		bool operator!=(const avl_iter<T>& lhs, const avl_iter<T>& rhs)
+		{
+			return !(lhs == rhs);
+		}
 	}
 
 	template <typename T>
@@ -91,7 +95,7 @@ namespace MySTL
 		auto k1 = k2->right_;
 		k2->right_ = k1->left_;
 		k1->left_ = k2;
-		k2->height_ = std::max(getHeight(k2->left_), getHeight(k2->right_))+1;
+		k2->height_ = std::max(getHeight(k2->left_), getHeight(k2->right_)) + 1;
 		k1->height_ = std::max(getHeight(k1->right_), k2->height_) + 1;
 		return k1;
 	}
@@ -130,14 +134,14 @@ namespace MySTL
 		if (p == 0) return;
 		if (p->data_ != val)
 		{
-			if (val > p->data_) return (val, p->right_);
-			else return (val, p->left_);
+			if (val > p->data_) return erase_elem(val, p->right_);
+			else return erase_elem(val, p->left_);
 		}
 		else
 		{
 			if (p->left_ != 0 && p->right_ != 0)
 			{
-				size_t choose = size_t % 2;
+				size_t choose = __size % 2;
 				auto pos = (choose == 0 ?
 					const_cast<node*>(find_min_aux(p->right_)) :
 					const_cast<node*>(find_max_aux(p->left_)));
@@ -155,8 +159,180 @@ namespace MySTL
 				--__size;
 			}
 		}
+		if (p != 0)
+		{
+			p->height_ = std::max(getHeight(p->left_), getHeight(p->right_)) + 1;
+			if (getHeight(p->left_) - getHeight(p->right_) == 2)
+			{
+				if (p->left_->right_ == 0) singleLeftLeftRotate(p);
+				//else doubleRightLeftRotate(p);
+				else doubleLeftRightRotate(p);
+			}
+			else if (getHeight(p->right_) - getHeight(p->left_) == 2)
+			{
+				if (p->right_->left_ == 0) singleRightRightRotate(p);
+				else doubleRightLeftRotate(p);
+			}
+		}
+	}
+	template <typename T>
+	void avl_Tree<T>::erase(const T& val)
+	{
+		erase_elem(val, __root);
+	}
+	template <typename T>
+	void avl_Tree<T>::insert_elem(const T& val, node*& p)
+	{
+		if (p == 0)
+		{
+			p = dataAllocator::allocate();
+			dataAllocator::construct(p);
+			p->data_ = val;
+			p->left_ = p->right_ = 0;
+			p->height_ = 1;
+			++__size;
+		}
+		else if (p->data_ < val)
+		{
+			insert_elem(val, p->right_);
+			if (getHeight(p->right_) - getHeight(p->left_) == 2)
+			{
+				if (val > p->right_->data_)
+					p = singleRightRightRotate(p);
+				else
+					p = doubleRightLeftRotate(p);
+			}
+		}
+		else if (p->data_ > val)
+		{
+			insert_elem(val, p->left_);
+			if (getHeight(p->left_) - getHeight(p->right_) == 2)
+			{
+				if (val < p->left_->data_)
+					p = singleLeftLeftRotate(p);
+				else p = doubleLeftRightRotate(p);
+			}
+		}
+		p->height_ = max(getHeight(p->left_), getHeight(p->right_)) + 1;
+	}
+	template <typename T>
+	void avl_Tree<T>::insert(const T& val)
+	{
+		insert_elem(val, __root);
+	}
+	template <typename T>
+	void avl_Tree<T>::print_preorder_aux(const string& delim, std::ostream& os,
+		const node* p) const
+	{
+		if (p)
+		{
+			os << p->data_ << delim;
+			print_preorder_aux(delim, os, p->left_);
+			print_preorder_aux(delim, os, p->right_);
+		}
+	}
+	template <typename T>
+	void avl_Tree<T>::print_preorder(const string& delim,
+		std::ostream& os) const
+	{
+		print_preorder_aux(delim, os, __root);
+	}
+	template <typename T>
+	void avl_Tree<T>::print_inorder_aux(const string& delim,
+		std::ostream& os, const node* ptr) const
+	{
+		if (ptr)
+		{
+			print_inorder_aux(delim, os, ptr->left_);
+			os << ptr->data_ << delim;
+			print_inorder_aux(delim, os, ptr->right_);
+		}
+	}
+	template <typename T>
+	void avl_Tree<T>::print_inorder(const string& delim,
+		std::ostream& os) const
+	{
+		print_inorder_aux(delim, os, __root);
+	}
+	template <typename T>
+	void avl_Tree<T>::print_postorder_aux(const string& delim,
+		std::ostream& os, const node* ptr) const
+	{
+		if (ptr)
+		{
+			print_postorder_aux(delim, os, ptr->left_);
+			print_postorder_aux(delim, os, ptr->right_);
+			os << ptr->data_ << delim;
+		}
+	}
+	template <typename T>
+	void avl_Tree<T>::print_postorder(const string& delim,
+		std::ostream& os) const
+	{
+		print_postorder_aux(delim, os, __root);
+	}
+	template <typename T>
+	void avl_Tree<T>::print_levelorder(const string& delim,
+		std::ostream& os) const
+	{
+		auto temp = __root;
+		if (temp != 0)
+		{
+			std::deque<node*> q;
+			q.push_back(temp);
+			while (!q.empty())
+			{
+				temp = q.front();
+				q.pop_front();
+				os << temp->data_ << delim;
+				if (temp->left_ != 0) q.push_back(temp->left_);
+				if (temp->right_ != 0) q.push_back(temp->right_);
+			}
+		}
+	}
+	template <typename T> //avl_iter<node>
+	typename avl_Tree<T>::const_iterator avl_Tree<T>::
+		find_aux(const T& val, const node* ptr) const
+	{
+		while (ptr != 0)
+		{
+			if (ptr->data_ < val)
+				ptr = ptr->right_;
+			else if (ptr->data_ > val)
+				ptr = ptr->left_;
+			else break;
+		}
+		return const_iterator(ptr, this);
+	}
+	template <typename T>
+	typename avl_Tree<T>::const_iterator avl_Tree<T>::
+		find(const T& val) const
+	{
+		return find_aux(val, __root);
 	}
 
+	template <typename T>
+	typename avl_Tree<T>::const_iterator avl_Tree<T>::
+		find_max_aux(const node* ptr) const
+	{
+		while (ptr != 0 && ptr->right_ != 0)
+			ptr = ptr->right_;
+		return const_iterator(ptr, this);
+	}
+	template <typename T>
+	typename avl_Tree<T>::const_iterator avl_Tree<T>::
+		find_max() const { return find_max_aux(__root); }
+	template <typename T>
+	typename avl_Tree<T>::const_iterator avl_Tree<T>::
+		find_min_aux(const node* ptr) const
+	{
+		while (ptr != 0 && ptr->left_ != 0)
+			ptr = ptr->left_;
+		return const_iterator(ptr, this);
+	}
+	template <typename T>
+	typename avl_Tree<T>::const_iterator avl_Tree<T>::
+		find_min() const { return find_min_aux(__root); }
 }
 
 #endif // !_AVLTREE_IMPL_H
