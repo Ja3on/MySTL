64770fd3e224b30821438c724ba0ca354dea790b
Ja3on <ja3on.he@gmail.com>
2016-06-15 12:43:41 +0800

    重构

--- Detail/TrieTree.cpp
+++ Detail/TrieTree.cpp
@@ -53,6 +53,84 @@ namespace MySTL
 	}
 	bool trie_tree::_insert(const string& word, const node_ptr& up)
 	{
+		if (word.empty())
+		{
+			++__size;
+			up->is_a_word = true;
+			return true;
+		}
+		char ch = word[0];
+		auto res = up->map_childs.find(ch);
+		if (res != up->map_childs.end())
+			return _insert(word.substr(1), res->second);
+		else
+		{
+			auto is_a_word = (word.size() == 1 ? true : false);
+			auto node = make_node(ch, is_a_word);
+			up->map_childs[ch] = std::move(node);
+			return _insert(word.substr(1), up->map_childs[ch]);
+		}
+	}
+	void trie_tree::print_tree(std::ostream& os) const
+	{
+		auto root = get_root();
+		if (root == nullptr)
+			os << "the trie_tree is empty!" << std::endl;
+		for (auto cit = root->map_childs.cbegin(); 
+			cit != root->map_childs.cend();	++cit)
+			_print_tree(os, cit->second, string());
+	}
+	void trie_tree::_print_tree(std::ostream& os, const node_ptr& up,
+		string word) const
+	{
+		word += up->data;
+		if (up->is_a_word)
+			os << word << std::endl;
+		for (auto cit = up->map_childs.cbegin();
+			cit != up->map_childs.cend(); ++cit)
+			_print_tree(os, cit->second, word);
+	}
+	vector<string> trie_tree::get_word_by_prefix(const string& prefix) const
+	{
+		vector<string> words;
+		auto root = get_root();
+		if (root == nullptr || prefix.size() == 0)
+			return words;
+		char ch = prefix[0];
+		auto result = root->map_childs.find(ch);
+		if (result != root->map_childs.end())
+			_get_word_by_prefix(prefix, result->second, prefix, words);
+		return words;
+	}
+	void trie_tree::_get_word_by_prefix(const string& prefix, const node_ptr& up,
+		const string& real_prefix, vector<string>& words) const
+	{
+		if (prefix.size() == 1)
+		{
+			if (up->is_a_word)
+				words.push_back(real_prefix);
+			for (auto cit = up->map_childs.cbegin(); cit != up->map_childs.cend();
+				++cit)
+				__get_word_by_prefix(cit->second, string(), real_prefix, words);
+		}
+		else
+		{
+			char ch = prefix[1];
+			auto res = up->map_childs.find(ch);
+			if (res != up->map_childs.end())
+				_get_word_by_prefix(prefix.substr(1), res->second, 
+					real_prefix, words);
+		}
+	}
 
+	void trie_tree::__get_word_by_prefix(const node_ptr& up, string& word,
+		const string& prefix, vector<string>& words) const
+	{
+		word += up->data;
+		if (up->is_a_word)
+			words.push_back(prefix + word);
+		for (auto cit = up->map_childs.cbegin(); cit != up->map_childs.cend();
+			++cit)
+			__get_word_by_prefix(cit->second, string(word), prefix, words);
 	}
 }
