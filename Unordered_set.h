c590e0fc42397eacf1404686bd579af713051484
Ja3on <ja3on.he@gmail.com>
2016-06-24 17:05:01 +0800

    RB_tree.h

--- Unordered_set.h
+++ Unordered_set.h
@@ -29,8 +29,8 @@ namespace MySTL
 				typename Allocator>
 				friend class Unordered_set;
 		private:
-			typedef usi_iterator<Key, Hash, KeyEqual, Allocator>* cntrPtr;
-			size_t _buket_index;
+			typedef Unordered_set<Key, Hash, KeyEqual, Allocator>* cntrPtr;
+			size_t _bucket_index;
 			ListIterator _iterator;
 			cntrPtr _container;
 		public:
@@ -75,10 +75,59 @@ namespace MySTL
 		typedef Detail::ust_iterator<Key, typename MySTL::list<key_type>::
 			iterator, Hash, KeyEqual, Allocator> iterator;
 	private:
-		MySTL::vector<MySTL::list<key_type>> __buket;
+		MySTL::vector<MySTL::list<key_type>> __bucket;
 		size_type __size;
 		float __max_load_factor;
+
+#define PRIME_LIST_SIZE 28
+		static size_t prime_list_[PRIME_LIST_SIZE];
+	public:
+		explicit Unordered_set(size_t bucket_count);
+		template <typename InputIterator>
+		Unordered_set(InputIterator first, InputIterator last);
+		Unordered_set(const Unordered_set&);
+		Unordered_set& operator =(const Unordered_set&);
+
+		size_type size() const;
+		bool empty() const;
+		size_type bucket_count() const;
+		size_type bucket_size(size_type) const;
+		size_type bucket(const key_type&) const;
+		//size_type bucket_index(const key_type& key) const;
+		float load_factor() const;
+		float max_load_factor() const;
+		void max_load_factor(float);
+		void rehash(size_type);
+
+		iterator begin();
+		iterator end();
+		local_iterator begin(size_type i);
+		local_iterator end(size_type);
+
+		iterator find(const key_type&);
+		size_type count(const key_type&);
+
+		MySTL::pair<iterator, bool> insert(const value_type&);
+		template <typename InputIterator>
+		void insert(InputIterator first, InputIterator last);
+		iterator erase(iterator position);
+		size_type erase(const key_type& key);
+
+		haser hash_function() const;
+		key_equal key_eq() const;
+		allocator_type get_allocator() const;
+ 
+	private:
+		size_type __next_prime(size_type n) const;
+		size_type __bucket_index(const key_type& key) const;
+		bool has_key(const key_type&);
+	public:
+		template <typename Key, typename Hash, typename KeyEqual,
+			typename Allocator>
+			friend void swap(Unordered_set<Key, Hash, KeyEqual, Allocator>& lhs,
+				Unordered_set<Key, Hash, KeyEqual, Allocator>& rhs);
 	};
 }
 
+#include "../MySTL/Detail/Unordered_set.impl.h"
 #endif // !_UNORDERED_SET_H
